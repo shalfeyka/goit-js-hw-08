992ccf91efe9f8b4daf32add62983d552871d41b
Marina Checheneva <m.checheneva140201@gmail.com>
2022-07-06 01:33:26 +0300

    done task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+const imgGallery = document.querySelector('.gallery');
+// console.log(imgGallery);
+
+const listImages = galleryItems
+.map(item => `<div class='gallery__item'>
+<a class="gallery__link" href=${item.original}>
+<img class='gallery__image'
+src = ${item.preview}
+alt = ${item.description}/>
+</a>
+</div>`).join('');
+// console.log(listImages)
+imgGallery.innerHTML = listImages;
+let lightbox = new SimpleLightbox('.gallery a', {captions: true, captionSelector: 'img', captionsData: 'alt', captionDelay: 250});
+// Change code below this line
+
+
