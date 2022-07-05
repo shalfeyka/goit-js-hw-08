43872eb1e53ace7f1724105fdeabb2527ec4867b
Marina Checheneva <m.checheneva140201@gmail.com>
2022-07-06 01:46:34 +0300

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+const keyStorage = 'videoplayer-current-time';
+const videoVimeo = document.querySelector('#vimeo-player');
+const video = new Video(videoVimeo);
+console.log(video);
+const localStrItem = localStorage.getItem(keyStorage);
+if (localStrItem){
+    video.setCurrentTime(localStorage.getItem(keyStorage));    
+} video.on('timeupdate', throttle(getTime,1000));
+
+function getTime() {
+video.getCurrentTime() .then(prom => {
+console.log(prom);
+localStorage.setItem(keyStorage,prom);
+});
+}
